e10cb6a1c7e6fbd02f72a43a0d97468730ba6f26
ebissa <114221753+ebissa@users.noreply.github.com>
2022-09-26 10:16:37 +0300

    Create numberofGoodPairs.py

---
+++ numberofGoodPairs.py
@@ -0,0 +1,9 @@
+import array as ar
+class Solution:
+    def numIdenticalPairs(self, nums):
+        output=0
+        for i in range(len(nums)):
+            for j in  range(len(nums)):
+                if nums[i] == nums[j] and i < j:
+                    output+=1
+        return output
