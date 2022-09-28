364b70a9a099133366dc506bc9ecdf1dbf03b06f
ebissa <114221753+ebissa@users.noreply.github.com>
2022-09-28 22:27:43 +0300

    Create countSort.py

---
+++ countSort.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    count=[0]*100
+    for item in (arr):
+        count[item]+=1
+    return count
+        
