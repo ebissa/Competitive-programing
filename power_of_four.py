968f360a6d102f886946d9ead927185e8a92aeaa
ebissa <114221753+ebissa@users.noreply.github.com>
2022-10-09 22:25:49 +0300

    Create power_of_four.py

---
+++ power_of_four.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n==1:
+            return True
+        elif n==0 or n%4!=0:
+            return False
+        else:
+            return self.isPowerOfFour(n//4)
+           
+            power-of-four
