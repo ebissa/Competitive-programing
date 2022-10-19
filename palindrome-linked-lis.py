4369411df8ba5268cbf5e42b74f99ff7559fdf29
ebissa <114221753+ebissa@users.noreply.github.com>
2022-10-19 23:38:47 +0300

    Create palindrome-linked-lis.py

---
+++ palindrome-linked-lis.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, head):
+        vals = []
+        current_node = head
+        while current_node is not None:
+            vals.append(current_node.val)
+            current_node = current_node.next
+        return vals == vals[::-1]
+       
