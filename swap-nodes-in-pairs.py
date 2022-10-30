d78cee8f4f225b635b574842d95863330a224926
ebissa <114221753+ebissa@users.noreply.github.com>
2022-10-30 23:47:54 +0300

    Create swap-nodes-in-pairs.py

---
+++ swap-nodes-in-pairs.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        dummy = ListNode(0, head)
+        prev, curr = dummy, head
+        while curr and curr.next:
+            nxtPair = curr.next.next
+            second = curr.next
+            
+            second.next = curr
+            curr.next = nxtPair
+            prev.next = second
+            
+            prev = curr
+            curr = nxtPair
+        return dummy.next
