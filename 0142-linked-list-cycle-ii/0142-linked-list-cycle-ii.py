020fd0f7835319128b8a633b9e22ff771c29d309
Ebissa Chemeda <114221753+ebissa@users.noreply.github.com>
2024-02-14 21:45:03 +0300

    Time: 41 ms (78.96%), Space: 19.1 MB (73.93%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -9,21 +9,16 @@ class Solution:
         
         if head is None:
             return None
-
-        # Step 1: Determine if there is a cycle
         slow = head
         fast = head
-        while fast is not None and fast.next is not None:
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             if slow == fast:
                 break
 
-        # If no cycle was found, return null
-        if fast is None or fast.next is None:
+        if not(fast) or not(fast.next):
             return None
-
-        # Step 2: Find the start of the cycle
         slow = head
         while slow != fast:
             slow = slow.next
