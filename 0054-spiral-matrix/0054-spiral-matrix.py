2dce9057ed4397ea441e9bc9f19edc09bba6cf98
Ebissa Chemeda <114221753+ebissa@users.noreply.github.com>
2024-02-17 16:18:52 +0300

    Time: 35 ms (64.59%), Space: 16.6 MB (78.64%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,26 +1,28 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        l, r = 0, len(matrix[0]) - 1
-        u, d = 0, len(matrix) - 1
-        res = []
-        step = 0
-        while l <= r and u <= d:
-            match (step % 4):
-                case 0:
-                    for i in range(l, r+1):
-                        res.append(matrix[u][i])
-                    u += 1
-                case 1:
-                    for i in range(u, d+1):
-                        res.append(matrix[i][r])
-                    r -= 1
-                case 2:
-                    for i in range(r, l-1, -1):
-                        res.append(matrix[d][i])
-                    d -= 1
-                case 3:
-                    for i in range(d, u-1, -1):
-                        res.append(matrix[i][l])
-                    l += 1
-            step += 1
+        l,r=0,len(matrix[0])-1
+        h,lo=0,len(matrix)-1
+        step=0
+        res=[]
+        while l<=r and h<=lo:
+            matcher=step%4
+            if matcher==0:
+                for i in range(l,r+1):
+                    res.append(matrix[h][i])
+                h+=1
+            elif matcher==1:
+                for i in range(h,lo+1):
+                    res.append(matrix[i][r])
+                r-=1
+            elif matcher==2:
+                for i in range(r,l-1,-1):
+                    res.append(matrix[lo][i])
+                lo-=1
+            elif matcher==3:
+                for i in range(lo,h-1,-1):
+                    res.append(matrix[i][l])
+                l+=1
+            step+=1
         return res
+                    
+        
