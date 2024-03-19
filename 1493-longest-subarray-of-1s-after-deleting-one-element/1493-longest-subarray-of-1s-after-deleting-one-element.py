cdc6ffa560cc2c1e01c127a988e6627d67414fa7
Ebissa Chemeda <114221753+ebissa@users.noreply.github.com>
2024-03-19 06:59:02 +0300

    Time: 482 ms (22.37%), Space: 19.4 MB (99.09%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -5,19 +5,16 @@ class Solution:
         max_size=0
         can_delete=True
         for r in range(len(nums)):
-            if nums[r]==1:
-                count+=1
-                max_size=max(max_size,count)
-            elif  nums[r]!=1 and can_delete==True:
-                can_delete=False
-                continue
             while nums[r]!=1 and can_delete==False:
                 if nums[l]==1:
                     count-=1
                 elif nums[l]!=1:
                     can_delete=True
                 l+=1
-            if  nums[r]!=1 and can_delete==True:
+            if nums[r]==1:
+                count+=1
+                max_size=max(max_size,count)
+            elif  nums[r]!=1 and can_delete==True:
                 can_delete=False
                 continue
             
