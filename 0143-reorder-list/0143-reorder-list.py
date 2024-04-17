5e154e69bc613ce30301aa4404d05229cfb46c38
Ebissa Chemeda <114221753+ebissa@users.noreply.github.com>
2024-04-17 06:37:05 +0300

    Time: 59 ms (37.11%), Space: 24.6 MB (86.47%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -5,27 +5,27 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
         slow,fast=head,head.next
         while fast and fast.next:
             slow=slow.next
             fast=fast.next.next
         second=slow.next
         prev=slow.next=None
+        
         while second:
             temp=second.next
             second.next=prev
             prev=second
             second=temp
-        first,second=head,prev
+        second=prev
+        first=head
         while second:
-            tem1,tem2=first.next,second.next
+            temp1,temp2=first.next,second.next
             first.next=second
-            second.next=tem1
-            first=tem1
-            second=tem2
+            second.next=temp1
+            first=temp1
+            second=temp2
+            
         
         
         
