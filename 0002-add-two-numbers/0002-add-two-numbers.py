0344de466d623b4f408b1c09e144045f729a9815
Ebissa Chemeda <114221753+ebissa@users.noreply.github.com>
2024-04-18 18:51:43 +0300

    Time: 47 ms (91.18%), Space: 16.7 MB (48.84%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,51 +5,19 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        before=ListNode(None)
-        dummy=before
-        leftover=0
-        while l1 and l2:
-            sums=l1.val+l2.val+leftover
-            if sums>=10:
-                remainder=sums%10
-                leftover=1
-            else:
-                remainder=sums
-                leftover=0
-            new=ListNode(remainder)
-            before.next=new
-            before=new
-            l1=l1.next
-            l2=l2.next
-        while l1:
-            sums=l1.val+leftover
-            if sums>=10:
-                remainder=sums%10
-                leftover=1
-            else:
-                remainder=sums
-                leftover=0
-            new=ListNode(remainder)
-            before.next=new
-            before=new
-            l1=l1.next
-        while l2:
-            sums=l2.val+leftover
-            if sums>=10:
-                remainder=sums%10
-                leftover=1
-            else:
-                remainder=sums
-                leftover=0
-            new=ListNode(remainder)
-            before.next=new
-            before=new
-            l2=l2.next
-        if leftover>0:
-            new=ListNode(leftover)
-            before.next=new
-            before=new
-        before.next=None
+        dummy=ListNode()
+        curr=dummy
+        carry=0
+        while l1 or l2 or carry:
+            v1=l1.val if l1 else 0
+            v2=l2.val if l2 else 0
+            sums=v1+v2+carry
+            carry=sums//10
+            val=sums%10
+            curr.next=ListNode(val)
+            curr=curr.next
+            l1=l1.next if l1 else None
+            l2=l2.next if l2 else None
             
         return dummy.next
             
