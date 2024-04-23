c5c29a562ab97d263f5700ad84e254053c9b74a2
Ebissa Chemeda <114221753+ebissa@users.noreply.github.com>
2024-04-23 18:08:24 +0300

    Time: 583 ms (45.05%), Space: 77.8 MB (64.24%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -26,10 +26,10 @@ class LRUCache:
 
 
     def get(self, key: int) -> int:
-        if self.cach[key]:
+        if key in self.cach:
             self.remove(self.cach[key])
             self.insert(self.cach[key])
-            return self.cach[key]
+            return self.cach[key].val
         else:
             return -1
 
@@ -42,56 +42,9 @@ class LRUCache:
         if len(self.cach)>self.cap:
             lru=self.left.next
             self.remove(lru)
-            del cach[lru.key]
+            del self.cach[lru.key]
 
-        
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
-
-class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
+    
 
 
-class LRUCache:
-    def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}  # map key to node
-
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
-
-    # remove node from list
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-
-    # insert node at right
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
-
-    def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-
-        if len(self.cache) > self.cap:
-            # remove from the list and delete the LRU from hashmap
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
+    
