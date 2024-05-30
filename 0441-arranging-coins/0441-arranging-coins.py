4fd9147ce73db6e2c0b207075586319606664376
Ebissa Chemeda <114221753+ebissa@users.noreply.github.com>
2024-05-30 17:34:11 +0300

    Time: 45 ms (55.07%), Space: 16.4 MB (98.32%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,6 +1,5 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        root=math.sqrt(1+8*n)
-        ans=math.floor((root-1)/2)
+        ans=math.floor((math.sqrt(1+8*n)-1)/2)
         return ans
         
